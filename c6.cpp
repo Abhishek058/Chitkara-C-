6d4592dea5ff34afc31a30348a4e48a0a3994727
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2022-03-30 16:07:27 +0530

    new commit

--- c6.cpp
+++ c6.cpp
@@ -1,17 +1,29 @@
 #include<iostream>
 using namespace std;
 
-void swap(int &a, int &b){
-    int temp = a;
-    a = b;
-    b = temp;
+//call by reference
+// void swap(int &a, int &b){
+//     int temp = a;
+//     a = b;
+//     b = temp;
+// }
+
+// call by pointer
+void swap(int *a, int *b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
 }
 
 int main()
 {
     int a, b;
     cin>>a>>b;
-    swap(a, b);
+    // swap(a, b);
+    cout<<a<<" "<<b<<endl;
+    swap(&a, &b);
     cout<<a<<" "<<b;
+
     return 0;
 }

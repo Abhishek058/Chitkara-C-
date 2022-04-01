bfaa4b0e1e4ef94afb8245787f97507852b2c583
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2022-04-01 12:31:41 +0530

    updated

--- c7.cpp
+++ c7.cpp
@@ -10,11 +10,39 @@ struct student
 
 int main()
 {
-    student s1;
-    cin>>s1.name;
-    cin>>s1.rollNo;
-    cin>>s1.marks;
+    // student s1;
+    // cin>>s1.name;
+    // cin>>s1.rollNo;
+    // cin>>s1.marks;
 
-    cout<<"Name: "<<s1.name<<", Roll No.:"<<s1.rollNo<<", Marks: "<<s1.marks;
+    // cout<<"Name: "<<s1.name<<", Roll No.:"<<s1.rollNo<<", Marks: "<<s1.marks;
+
+    student s2[5];
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> s2[i].name;
+        cin >> s2[i].rollNo;
+        cin >> s2[i].marks;
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        cout << s2[i].name << " ";
+        cout << s2[i].rollNo << " ";
+        cout << s2[i].marks << " ";
+        cout << endl;
+    }
+
+    int max = s2[0].marks;
+    int index = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        if (s2[i].marks > max)
+        {
+            max = s2[i].marks;
+            index = i;
+        }
+    }
+    cout<<"maximum marks scored by "<<s2[index].name<<": ";
+    cout<<max;
     return 0;
 }

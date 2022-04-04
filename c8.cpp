16cfe14d7ab9dafa5e686f3ee4be138dc1318a90
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2022-04-04 10:30:03 +0530

    Updated

--- c8.cpp
+++ c8.cpp
@@ -3,7 +3,14 @@ using namespace std;
 
 int main()
 {
-    int n;
+    int n, d;
     cin>>n;
-    for(int i = 0;)
+    int arr[n];
+    int result = 1;
+    for(int i = 0; i < n; i++){
+        cin>>arr[i];
+        result *= arr[i];
+    }
+    cin>>d;
+    cout<<result%d;
 }

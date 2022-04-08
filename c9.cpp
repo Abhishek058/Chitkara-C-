6ae1bdf7a00be7ca19a1f3e024a332d2e25654e5
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2022-04-08 12:03:27 +0530

    updated

--- c9.cpp
+++ c9.cpp
@@ -1,19 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int main()
+void print2d(int n, int m, int arr[][3])
 {
-    int n, m;
-    cin >> n >> m;
-    int arr[n][m];
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            cin >> arr[i][j];
-        }
-    }
-    cout << endl;
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
@@ -22,5 +11,19 @@ int main()
         }
         cout << endl;
     }
+}
+int main()
+{
+    int arr[3][3];
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    cout << endl;
+
+    print2d(3, 3, arr);
     return 0;
 }

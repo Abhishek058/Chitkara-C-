efffe8a1a3867bd739ffa16fa606c22d10ca9a4d
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2022-04-08 12:21:29 +0530

    bug fixed

--- c9.cpp
+++ c9.cpp
@@ -12,18 +12,36 @@ void print2d(int n, int m, int arr[][3])
         cout << endl;
     }
 }
+
+void transArr(int arr[][3])
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            int temp = arr[i][j];
+            arr[i][j] = arr[j][i];
+            arr[j][i] = temp;
+        }
+    }
+}
 int main()
 {
-    int arr[3][3];
+    int arr1[3][3];
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 3; j++)
         {
-            cin >> arr[i][j];
+            cin >> arr1[i][j];
         }
     }
     cout << endl;
 
-    print2d(3, 3, arr);
+    print2d(3, 3, arr1);
+    cout << endl;
+    cout<<"After Transpose:"<<endl;
+    transArr(arr1);
+    cout << endl;
+    print2d(3, 3, arr1);
     return 0;
 }

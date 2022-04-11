f5bcdfea4f4efe662f591bdd909a8e2f061e9547
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2022-04-11 11:22:37 +0530

    updated

--- c10.cpp
+++ c10.cpp
@@ -1,8 +1,31 @@
 #include<iostream>
 using namespace std;
 
+struct employee
+{
+    int empId;
+    char ename[20];
+    int salary;
+};
+
+
+
 int main()
 {
-    
+    employee e1[5];
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> e1[i].empId;
+        cin >> e1[i].ename;
+        cin >> e1[i].salary;
+    }
+
+    int maxSal = 0;
+    for(int i=0; i<5; i++){
+        if(e1[i].salary > maxSal){
+            maxSal = e1[i].salary;
+        }
+    }
+    cout<<"maximum slary of employee is "<< maxSal;
     return 0;
 }

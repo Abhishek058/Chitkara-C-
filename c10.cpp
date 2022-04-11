e83e162d175e8ec24f7ee26fda0cbf66e3968dee
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2022-04-11 12:09:18 +0530

    updated

--- c10.cpp
+++ c10.cpp
@@ -8,25 +8,45 @@ struct employee
     int salary;
 };
 
+void digit(int n)
+{
+    int cn = n;
+    int digi = 0;
+    while (n > 0)
+    {
+        int x = n%10;
+        digi = digi*10+x;
+        n = n/10;
+    }
+    if(digi = cn){
+        cout<<"integer is palindrome";
+    }
+    else{
+        cout<<"integer is not palindrome";
+    }
+}
+
 int main()
 {
-    employee e1[5];
-    for (int i = 0; i < 5; i++)
-    {
-        cin >> e1[i].empId;
-        cin >> e1[i].ename;
-        cin >> e1[i].salary;
-    }
+    // employee e1[5];
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     cin >> e1[i].empId;
+    //     cin >> e1[i].ename;
+    //     cin >> e1[i].salary;
+    // }
 
-    int maxSal = 0;
-    for (int i = 0; i < 5; i++)
-    {
-        if (e1[i].salary > maxSal)
-        {
-            maxSal = e1[i].salary;
-        }
-    }
+    // int maxSal = 0;
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     if (e1[i].salary > maxSal)
+    //     {
+    //         maxSal = e1[i].salary;
+    //     }
+    // }
 
-    cout << "maximum slary of employee is " << maxSal;
+    // cout << "maximum slary of employee is " << maxSal;
+
+    digit(2332);
     return 0;
 }

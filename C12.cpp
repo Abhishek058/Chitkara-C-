698165d66abd32a6eacf13aa5ed8e5cca9a10686
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2022-04-13 15:57:46 +0530

    new file c12 added

---
+++ C12.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+class Counter{
+    int count;
+    public:
+        void set(){
+            count = 0;
+        }
+        void inc(){
+            count++;
+        }
+        void display() const{
+            count = count + 2;
+            cout<<count;
+        }
+        
+};
+
+int main()
+{
+    const Counter c1;
+    c1.set();
+    c1.inc();
+    c1.display();
+    return 0;
+}

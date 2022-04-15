2f631f91ad97859f39d2486da39f1e6651b9f623
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2022-04-15 11:57:16 +0530

    new file c13 added

---
+++ c13.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class Counter
+{
+    int count;
+
+public:
+    void set()
+    {
+        count = 0;
+    }
+    void inc()
+    {
+        count++;
+    }
+    void display() const
+    {
+        // count = count + 2;
+        cout << count;
+    }
+};
+
+int
+main()
+{
+    Counter c1;
+    c1.set();
+    c1.inc();
+    c1.inc();
+    c1.display();
+    return 0;
+}

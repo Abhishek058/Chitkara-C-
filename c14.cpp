06a96ba19c396f59a309f3ad3767b15b39614b44
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2022-04-18 11:23:40 +0530

    max, min, avg function added

--- c14.cpp
+++ c14.cpp
@@ -12,9 +12,34 @@ public:
         b = y;
     }
     friend float mean(Integer);
+    friend void max(Integer);
+    friend void min(Integer);
+    friend float avg(Integer);
 };
 
 float mean(Integer obj)
+{
+    return (obj.a + obj.b) / 2;
+}
+void max(Integer obj)
+{
+    if(obj.a > obj.b){
+        cout<<obj.a<<endl;
+    }
+    else{
+        cout<<obj.b<<endl;
+    }
+}
+void min(Integer obj)
+{
+    if(obj.a > obj.b){
+        cout<<obj.b<<endl;
+    }
+    else{
+        cout<<obj.a<<endl;
+    }
+}
+float avg(Integer obj)
 {
     return (obj.a + obj.b) / 2;
 }
@@ -25,6 +50,11 @@ int main()
     Integer obj;
     cin>>i>>j;
     obj.setVal(i, j);
+    cout<<mean(obj)<<endl;
+    max(obj);
+    cout<<endl;
+    min(obj);
+    cout<<endl;
     cout<<mean(obj);
     return 0;
 }

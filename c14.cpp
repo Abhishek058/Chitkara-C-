70f6e62793232dbf8345562e41d422bf109e6a6c
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2022-04-18 11:06:22 +0530

    integer class added

---
+++ c14.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class integer
+{
+    int a, b;
+
+public:
+    void setVal(int x, int y)
+    {
+        a = x;
+        b = y;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}

e43f8bbe94b7718fa5000ff8fd1108b3d119d03c
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2022-04-20 15:39:42 +0530

    Point class added and code updated

--- c16.cpp
+++ c16.cpp
@@ -1,10 +1,20 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
+class Point
+{
+    int x, y;
 
+public:
+    Point(Point &p)
+    {
+        x = p.x;
+        y = p.y;
+    }
+};
 
 int main()
 {
-    
+
     return 0;
 }

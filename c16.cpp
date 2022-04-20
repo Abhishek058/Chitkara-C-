9ecac441cefcd384136f392a83338a5f0f0fcad2
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2022-04-20 15:45:49 +0530

    set and display function added

--- c16.cpp
+++ c16.cpp
@@ -6,15 +6,27 @@ class Point
     int x, y;
 
 public:
+    Point() {}
     Point(Point &p)
     {
         x = p.x;
         y = p.y;
     }
+    void set(int a, int b)
+    {
+        x = a;
+        y = b;
+    }
+    void disp()
+    {
+        cout << "X: " << x << " Y: " << y;
+    }
 };
 
 int main()
 {
-
+    Point p1;
+    p1.set(26, 43);
+    p1.disp();
     return 0;
 }

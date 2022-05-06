36c543cc9956d2842182ec4edce771a2ff7e242b
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-05-06 13:47:40 +0530

    basic code for distance class added

---
+++ c18.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+class distance{
+    int feet, inches;
+    public:
+    distance(){
+        feet = inches = 0;
+    }
+    distance(int f, int i){
+        feet = f;
+        inches = i;
+    }
+}
+
+int main()
+{
+    
+    return 0;
+}

8d32267f2439f82961b260626bdc152bda8debc9
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-05-11 10:35:28 +0530

    student result class added

--- c20.cpp
+++ c20.cpp
@@ -60,29 +60,79 @@ class D: public A{
         cout<<"Z = "<<z<<endl;
     }
 };
+class student{
+    int rollno;
+    public:
+    void setroll(){
+        cin>>rollno;
+    }
+    void disproll(){
+        cout<<"Roll NO. = "<<rollno<<endl;
+    }
+};
+class test{
+    protected:
+    int sub1, sub2, sub3;
+    public:
+    void setmarks(){
+        cout<<"Sub1 marks: ";
+        cin>>sub1;
+        cout<<"Sub2 marks: ";
+        cin>>sub2;
+        cout<<"Sub3 marks: ";
+        cin>>sub3;
+    }
+    void dispmarks(){        
+        cout<<"Sub1 marks: "<<sub1<<endl;
+        cout<<"Sub2 marks: "<<sub2<<endl;
+        cout<<"Sub3 marks: "<<sub3<<endl;
+    }
+};
+
+class result: public student, public test{
+    int avg; 
+    public:
+    void dispavg(){
+        avg = (sub1+sub2+sub3)/3;
+        cout<<"Average Marks: "<<avg<<endl;
+    }
+    void set(){
+        setroll();
+        setmarks();
+    }
+    void dispdetail(){
+        cout<<"***************Details*****************"<<endl;
+        disproll();
+        dispmarks();
+        dispavg();
+    }
+};
 
 int main()
 {
-    B obj1;
-    obj1.x; //error
-    obj1.y; //error
-    obj1.z; //error
-    obj1.set();
-    obj1.disp();
+    // B obj1;
+    // obj1.x; //error
+    // obj1.y; //error
+    // obj1.z; //error
+    // obj1.set();
+    // obj1.disp();
 
-    C obj2;
-    obj2.x; //error
-    obj2.y; //error
-    obj2.z; //error
-    obj2.set();
-    obj2.disp();
+    // C obj2;
+    // obj2.x; //error
+    // obj2.y; //error
+    // obj2.z; //error
+    // obj2.set();
+    // obj2.disp();
 
-    D obj3;
-    obj3.x; //error
-    obj3.y; //error
-    obj3.z; //error
-    obj3.set();
-    obj3.disp();
+    // D obj3;
+    // obj3.x; //error
+    // obj3.y; //error
+    // obj3.z; //error
+    // obj3.set();
+    // obj3.disp();
 
+    result r1;
+    r1.set();
+    r1.dispdetail();
     return 0;
 }

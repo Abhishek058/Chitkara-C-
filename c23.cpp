7a2bf420799ad4065d0cf82a478ee478c3556775
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-05-18 09:41:50 +0530

    Code done for finding area

--- c23.cpp
+++ c23.cpp
@@ -1,29 +1,84 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class shape{
-    char ch;
-    public:
-    void select(){
-        cin>>ch;
+class Shape
+{
+public:
+    virtual void area() = 0;
+    virtual void set() = 0;
+};
+
+class Tri : public Shape
+{
+    // Triangle is right angle triangle
+    int a, b, result;
+
+public:
+    void set()
+    {
+        cout << "set length: ";
+        cin >> a >> b;
+    }
+    void area()
+    {
+        result = (a * b) / 2;
+        cout << "Area of Triangle: " << result;
     }
 };
 
-class tri: public shape{
-    //Triangle is right angle triangle
-    int a, b, result;
-    public:
-    void set(){
-        cout<<"set length: ";
-        cin>>a>>b;
+class Sqr : public Shape
+{
+    int a, result;
+
+public:
+    void set()
+    {
+        cout << "set length: ";
+        cin >> a;
     }
-    void area(){
-        result = (a*b)/2;
+    void area()
+    {
+        result = (a * a);
+        cout << "Area of Square: " << result;
+    }
+};
+
+class Rect : public Shape
+{
+    int a, b, result;
+
+public:
+    void set()
+    {
+        cout << "set length: ";
+        cin >> a >> b;
+    }
+    void area()
+    {
+        result = a * b;
+        cout << "Area of Reactangle: " << result;
     }
 };
 
 int main()
 {
-    
+    // For Triangle
+    Shape *s;
+    Tri t;
+    s = &t;
+    s->set();
+    s->area();
+
+    // For Square
+    //  Sqr s;
+    //  s = &s;
+    //  s->set();
+    //  s->area();
+
+    // For Rectangle
+    //  Rect r;
+    //  s = &r;
+    //  s->set();
+    //  s->area();
     return 0;
 }

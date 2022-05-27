57addf475c9f79d3b68e6f6049520888b6b5a0e3
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-05-27 14:22:44 +0530

    code formated

--- c29.cpp
+++ c29.cpp
@@ -1,19 +1,25 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
 int main()
 {
-    //Standard Template Library
+    // Standard Template Library
     vector<int> v1;
-    cout<<v1.size()<<endl;
-    vector<int> v2 = {1, 2, 3 , 4, 5, 6};
-    cout<<v2.size()<<endl;
+    cout << v1.size() << endl;
+
+    vector<int> v2 = {1, 2, 3, 4, 5, 6};
+    cout << v2.size() << endl;
+
     vector<int> v3(5, 11);
-    cout<<v3.size()<<endl;
-    for(int i=0; i<5; i++){
-        cout<<v3[i]<<" ";
-    }cout<<endl;
-    cout<<v2[4]<<endl;
+    cout << v3.size() << endl;
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << v3[i] << " ";
+    }
+    cout << endl;
+    
+    cout << v2[4] << endl;
     return 0;
 }

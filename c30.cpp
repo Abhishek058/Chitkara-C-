f7bbc949a4bf55849509652e3ec63b048bb020e4
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-06-30 09:43:21 +0530

    prime number program added

--- c30.cpp
+++ c30.cpp
@@ -119,3 +119,30 @@
 //     return 0;
 // }
 
+// #include <iostream>
+// using namespace std;
+
+// bool prime_number(int n)
+// {
+
+//     if (n <= 1)
+//     {
+//         return false;
+//     }
+//     for (int i = 2; i < n; i++)
+//     {
+//         if (n % i == 0)
+//         {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     cout << prime_number(n);
+//     return 0;
+// }

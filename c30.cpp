38dc5f4d456bf4af0b56ef97a7e9d9ed8a24bfd6
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-07-13 10:05:03 +0530

    code completed now time to relax for a while

--- c30.cpp
+++ c30.cpp
@@ -305,37 +305,37 @@
 //     return 0;
 // }
 
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int binarySearch(int arr[], int key, int n)
-{
-    int s = 0;
-    int e = n - 1;
-    int mid = s + (e - s) / 2;
+// int binarySearch(int arr[], int key, int n)
+// {
+//     int s = 0;
+//     int e = n - 1;
+//     int mid = s + (e - s) / 2;
 
-    while (s <= e)
-    {
-        if (arr[mid] == key)
-        {
-            return mid;
-        }
-        if (arr[mid] < key)
-        {
-            s = mid + 1;
-        }
-        else
-        {
-            e = mid - 1;
-        }
-        mid = s + (e - s) / 2;
-    }
-    return -1;
-}
+//     while (s <= e)
+//     {
+//         if (arr[mid] == key)
+//         {
+//             return mid;
+//         }
+//         if (arr[mid] < key)
+//         {
+//             s = mid + 1;
+//         }
+//         else
+//         {
+//             e = mid - 1;
+//         }
+//         mid = s + (e - s) / 2;
+//     }
+//     return -1;
+// }
 
-int main()
-{
-    int arr[7] = {12, 13, 21, 23, 34, 45, 76};
-    cout << binarySearch(arr, 13, 7);
-    return 0;
-}
+// int main()
+// {
+//     int arr[7] = {12, 13, 21, 23, 34, 45, 76};
+//     cout << binarySearch(arr, 45, 7);
+//     return 0;
+// }

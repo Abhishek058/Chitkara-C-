3a507607af096e0cb0c92d228578ec0dd74a276d
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-07-21 09:39:44 +0530

    merge sort code added in class

--- c34.cpp
+++ c34.cpp
@@ -51,6 +51,65 @@ public:
         }
         PrintArray(arr, n);
     }
+    void merge(int arr[], int l, int mid, int r)
+    {
+        int n1 = mid - l + 1;
+        int n2 = r - mid;
+
+        int a[n1];
+        int b[n2]; // temperory array
+        for (int i = 0; i < n1; i++)
+        {
+            a[i] = arr[l + i];
+        }
+        for (int i = 0; i < n2; i++)
+        {
+            b[i] = arr[mid + 1 + i];
+        }
+
+        int i = 0;
+        int j = 0;
+        int k = l;
+        while (i < n1 && j < n2)
+        {
+            if (a[i] < b[j])
+            {
+                arr[k] = a[i];
+                k++;
+                i++;
+            }
+            else
+            {
+                arr[k] = b[j];
+                k++;
+                j++;
+            }
+        }
+        while (i < n1)
+        {
+            arr[k] = a[i];
+            k++;
+            i++;
+        }
+        while (j < n2)
+        {
+            arr[k] = b[j];
+            k++;
+            j++;
+        }
+    }
+    void mergesort(int arr[], int l, int r)
+    {
+        if (l < r)
+        {
+            int mid = (l + r) / 2;
+            mergesort(arr, l, mid);
+            mergesort(arr, mid + 1, r);
+
+            merge(arr, l, mid, r);
+        }
+        PrintArray(arr, n);
+    }
 };
 
 int main()
@@ -60,7 +119,8 @@ int main()
     int choice;
     cout << "Select Choice: " << endl;
     cout << "1. Insertion Sort" << endl
-         << "2. Selection Sort" << endl;
+         << "2. Selection Sort" << endl
+         << "2. Merge Sort" << endl;
     cin >> choice;
     switch (choice)
     {
@@ -70,6 +130,10 @@ int main()
     case 2:
         s1.SelectionSort(arr, 5);
         break;
+    case 3:
+        s1.mergesort(arr, 0, 5);
+        break;
     }
+
     return 0;
 }
